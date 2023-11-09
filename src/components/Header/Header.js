d6c5649cb3e73da2897c9d495fa9536239a95a8c
v662-coder <83722974+v662-coder@users.noreply.github.com>
2023-11-09 18:01:34 +0530

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import resumeSvg from "../../assets/resume.svg";
+
+import styles from "./Header.module.css";
+
+function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <p className={styles.heading}>
+          Hi👋, I am<span> Vishnu Chaurasiya </span> and i am helping you to
+        </p>
+        <p className={styles.heading}>
+          make your own resume. <span>It's free</span>
+        </p>
+      </div>
+      <div className={styles.right}>
+        <img src={resumeSvg} alt="Resume" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
